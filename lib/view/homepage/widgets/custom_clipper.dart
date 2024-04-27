81b1ba1677dcff8df6048b7a2def4fa348574758
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-04-27 22:51:07 +0600

    designed-add new clipper path design

--- lib/view/homepage/widgets/custom_clipper.dart
+++ lib/view/homepage/widgets/custom_clipper.dart
@@ -15,21 +15,19 @@ class CustomCurved extends CustomClipper<Path>{
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.moveTo(size.width*0.0024267,size.height*1.0129310);
-    path.quadraticBezierTo(size.width*0.0022133,size.height*0.5796897,size.width*0.0069867,size.height*0.4097241);
-    path.cubicTo(size.width*0.0838667,size.height*0.2749310,size.width*0.2801867,size.height*0.2880690,size.width*0.3821067,size.height*0.2741034);
-    path.cubicTo(size.width*0.3891733,size.height*0.3487931,size.width*0.4508000,size.height*0.3953448,size.width*0.4989600,size.height*0.3931724);
-    path.cubicTo(size.width*0.5764533,size.height*0.3821379,size.width*0.6020800,size.height*0.3226552,size.width*0.6155733,size.height*0.2757931);
-    path.cubicTo(size.width*0.7073600,size.height*0.2862759,size.width*0.8163200,size.height*0.2807241,size.width*1.0070933,size.height*0.4108966);
-    path.quadraticBezierTo(size.width*1.0061867,size.height*0.5833103,size.width*1.0054400,size.height*1.0047586);
-    path.lineTo(size.width*0.0024267,size.height*1.0129310);
+    path.moveTo(size.width*0.4950933,size.height*0.0123694);
+    path.cubicTo(size.width*0.5307200,size.height*0.0114094,size.width*0.5846400,size.height*0.0478898,size.width*0.5860800,size.height*0.1457741);
+    path.cubicTo(size.width*0.5935200,size.height*0.2296274,size.width*0.5467200,size.height*0.3007791,size.width*0.5003467,size.height*0.2360521);
+    path.cubicTo(size.width*0.4647467,size.height*0.2998191,size.width*0.4014400,size.height*0.2549939,size.width*0.4080267,size.height*0.1506111);
+    path.cubicTo(size.width*0.4072267,size.height*0.0962966,size.width*0.4328533,size.height*0.0139940,size.width*0.4950933,size.height*0.0123694);
 
-    path.moveTo(size.width*0.4937867,size.height*0.1500345);
-    path.cubicTo(size.width*0.5275200,size.height*0.1494828,size.width*0.5784267,size.height*0.1804138,size.width*0.5792000,size.height*0.2618621);
-    path.cubicTo(size.width*0.5796267,size.height*0.3071379,size.width*0.5549600,size.height*0.3754483,size.width*0.4959200,size.height*0.3763793);
-    path.cubicTo(size.width*0.4621867,size.height*0.3768966,size.width*0.4112533,size.height*0.3437241,size.width*0.4105067,size.height*0.2645172);
-    path.cubicTo(size.width*0.4100800,size.height*0.2192759,size.width*0.4347733,size.height*0.1509655,size.width*0.4937867,size.height*0.1500345); path.close();
-
+    path.moveTo(0,size.height*0.9969353);
+    path.quadraticBezierTo(0,size.height*0.4403131,0,size.height*0.2547724);
+    path.cubicTo(size.width*0.0939467,size.height*0.1278293,size.width*0.2845333,size.height*0.0129232,size.width*0.4085333,size.height*0.0332312);
+    path.cubicTo(size.width*0.4088800,size.height*0.0567515,size.width*0.3254667,size.height*0.2988960,size.width*0.4965333,size.height*0.3116346);
+    path.cubicTo(size.width*0.6649333,size.height*0.3114500,size.width*0.5860000,size.height*0.0544622,size.width*0.5840000,size.height*0.0350774);
+    path.cubicTo(size.width*0.8401333,size.height*0.0518776,size.width*0.9310667,size.height*0.1508326,size.width*0.9994667,size.height*0.2503415);
+    path.quadraticBezierTo(size.width*0.9989333,size.height*0.4360669,size.width*0.9973333,size.height*0.9932430);
     return path;
   }
 
