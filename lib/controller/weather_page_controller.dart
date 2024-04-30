193ff147e555fee6cf09f3ff5b57fa505cca2f2c
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-04-30 20:23:37 +0600

    update-weather_page_controller.dart

--- lib/controller/weather_page_controller.dart
+++ lib/controller/weather_page_controller.dart
@@ -9,10 +9,12 @@ class WeatherPageController extends ChangeNotifier{
   bool _showTodayData=true;
   bool _showNextDaysHourData=true;
   int? _forecastDayIndex;
+  bool _isCelsius = true;
 
 
   bool get showNextDaysHourData =>_showNextDaysHourData;
   bool get showTodayData => _showTodayData;
+  bool get isCelsius => _isCelsius;
   int? get forecastDayIndex => _forecastDayIndex;
   bool? get isLoading => _isLoading;
   WeatherResponseModel? get weatherResponseModelData => _weatherResponseModel;
@@ -31,6 +33,10 @@ class WeatherPageController extends ChangeNotifier{
     _showNextDaysHourData = showHourData;
     notifyListeners();
   }
+  void toggleTemperatureUnit() {
+    _isCelsius = !_isCelsius; // Toggle between Celsius and Fahrenheit
+    notifyListeners();
+  }
 
   Future<void>? getWeatherResponse({required String city})async{
     _isLoading=true;
@@ -41,7 +47,31 @@ class WeatherPageController extends ChangeNotifier{
     });
     notifyListeners();
   }
+  double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
 
+  // Method to convert Celsius to Fahrenheit
+  double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  // Method to get the current temperature with the appropriate unit
+  double getCurrentTemperature(num? temperature) {
+    if (temperature == null) {
+      return 0.0; // Or handle null value in another appropriate way
+    }
+    if (_isCelsius) {
+      return temperature.toDouble();
+    } else {
+      return celsiusToFahrenheit(temperature.toDouble());
+    }
+  }
+
+  // Method to get the unit label (C or F)
+  String getUnitLabel() {
+    return _isCelsius ? 'C' : 'F';
+  }
 
 }
 
